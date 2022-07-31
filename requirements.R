18c73b1dc18a93667c0316884707f993dcfa8e6e
Igo da Costa Andrade <61511751+igoandrade@users.noreply.github.com>
2022-07-31 16:39:09 -0300

    Create requirements.R

---
+++ requirements.R
@@ -0,0 +1,3 @@
+packages = c('HSAUR', 'PerformanceAnalytics', 'PortfolioAnalytics', 'RSQLite', 'caret', 'data.table', 'dplyr',  'e1071', 'ggplot2', 'ggraph', 'kableExtra', 'knitr', 'languageserver', 'lubridate', 'magrittr', 'markdown', 'mlr3', 'moments', 'openxlsx', 'purrr', 'quantmod', 'readr', 'readxl', 'reshape', 'reshape2', 'reticulate', 'rmarkdown', 'scales', 'shiny', 'stringr', 'tibbletime', 'tidyquant', 'tidyr', 'tidyverse', 'timetk', 'tinytex', 'xml')
+
+install.packages(packages)
